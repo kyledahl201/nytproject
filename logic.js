c129a275c62bc9caa1069bffbb66b211cb6f0cd4
Jonathan Andrews <ionathas78@hotmail.com>
2020-03-27 11:04:43 -0500

    Foundation work in JS file

--- logic.js
+++ logic.js
@@ -1,8 +1,16 @@
 //  **  Declaration
 
+var apiKey = "";                    //  Here's the API Key we're gonna be using for our requests
+var searchTerm = "";                //  The search term we'll be using in our request
 
 //  **  Functions
 
+function getAjax(queryURL, responseCall) {
+    $.ajax({
+        method: "GET",
+        url: queryURL
+    }).then(responseCall(response));
+}
 
 //  **  Event Calls
 
