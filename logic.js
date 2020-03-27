c90d9ef03ea0a8b4e59c2a15ccaaee5063514948
Jonathan Andrews <ionathas78@hotmail.com>
2020-03-27 11:11:10 -0500

    Forgot comments. Sorry!

--- logic.js
+++ logic.js
@@ -5,6 +5,12 @@ var searchTerm = "";                //  The search term we'll be using in our re
 
 //  **  Functions
 
+/**
+ * Execute an Ajax API call
+ * 
+ * @param {*} queryURL Full URL for the API query
+ * @param {*} responseCall Function to execute when the Ajax response comes in. 'function myFunction (response) {};'
+ */
 function getAjax(queryURL, responseCall) {
     $.ajax({
         method: "GET",
