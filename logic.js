9c7fb5574e33ceadffa3d147e4d1cf6bf5868c9b
Jonathan Andrews <ionathas78@hotmail.com>
2020-03-27 14:14:56 -0500

    Backend Code

--- logic.js
+++ logic.js
@@ -7,7 +7,6 @@ var _queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=elec
 
 
 
-
 //  **  Functions
 
 /**
@@ -45,12 +44,76 @@ function consoleLogIt(response) {
 // tryMeOut();
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=gAE9qvl59oWORBff4NG2zgSXSHlBluIm"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-}); 
+var queryString = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+var apiKey = "api-key=gAE9qvl59oWORBff4NG2zgSXSHlBluIm"
+
+var authorName = "";
+var headLine = "";
+var numberOfResults = 5;
+var searchResults = [];
+
+function queryAPI() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // console.log(response);
+        // searchResults = [];
+        for (var i = 0; i < numberOfResults; i++) {
+            let resultObject = {
+                headline: response.response.docs[i].headline.main,
+                author: response.response.docs[i].byline.original
+            };
+
+            searchResults.push(resultObject);
+        };
+    }); 
+};
+
+function apiSearchTerm(searchTerm) {
+    return "q=" + searchTerm;
+}
+
+function apiDates(beginDate, endDate) {
+    var returnString = ""
+
+    if ((beginDate !== undefined) && (beginDate !== null)) {
+        returnString += "begin_date=" + beginDate;
+    }
+    if ((endDate !== undefined) && (endDate !== null)) {
+        if (returnString != "") {
+            returnString += "&";
+        }
+        returnString += "end_date=" + endDate;
+    }
+    return returnString;
+}
+
+function buildQuery(searchTerm, beginDate, endDate) {
+    var returnString = queryString;
+
+    var searchQuery = "";
+    var dateQuery = "";
+
+
+    if (searchTerm !== null) {
+        returnString  += "&" + apiSearchTerm(searchTerm);
+    };
+    if ((beginDate !== null) || (endDate !== null)) {
+        returnString += "&" + apiDates(beginDate, endDate);
+    };
+
+    returnString += "&" + apiKey;
+    return returnString;
+}
+
+var testQuery = buildQuery("election", "19900101", "20100101");
+testQuery = testQuery.replace(/ /g, "+");
+
+queryAPI(testQuery);
+
+console.log(searchResults);
+
 
 // var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=gAE9qvl59oWORBff4NG2zgSXSHlBluIm"
 // $.ajax({
